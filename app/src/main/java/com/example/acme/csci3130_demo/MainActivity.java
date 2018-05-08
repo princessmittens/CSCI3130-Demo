9d876186e3519c7d3a46cf7b9833b2bdf1768e58
princessmittens <an747455@dal.ca>
2018-05-08 13:59:26 -0300

    added changes

--- app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130_demo/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.acme.csci3130_demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView viewTxt;
+    EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = findViewById(R.id.editTxt);
+        viewTxt = findViewById(R.id.viewTxt);
+        btn = findViewById(R.id.btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = edit.getText().toString();
+                viewTxt.setText(s);
+            }
+        });
     }
 }
